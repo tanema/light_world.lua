27b40830ad3cfb886673a21b249613f8ebefc629
Jakub Bucko <bucko.jakub7@gmail.com>
2019-06-20 19:27:57 +0200

    correction

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.identity = nil                   -- The name of the save directory (string)
-    t.version = "11.1"                -- The LÖVE version this game was made for (string)
+    t.version = "11.2"                -- The Lï¿½VE version this game was made for (string)
     t.console = true                   -- Attach a console (boolean, Windows only)
 
     t.window.title = "Untitled"        -- The window title (string)
