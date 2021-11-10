b3527f188ae16e33c6ecddd17a9845c576feb2bb
Tim Anema <tim.anema@agilebits.com>
2021-11-09 20:30:27 -0500

    small update

--- main.lua
+++ main.lua
@@ -1,5 +1,3 @@
-local mainlist = require('examples.examples_list')
-
 function love.load()
-  mainlist.start("examples_list.lua")
+  require('examples.examples_list').start("examples_list.lua")
 end
